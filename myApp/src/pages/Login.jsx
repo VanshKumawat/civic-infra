ad437bf6200099db7c3cc94ef958e8d6e707e7a2
VanshKumawat <kumarkumawat09@gmail.com>
2026-05-02 18:03:56 +0530

    2nd commit

--- myApp/src/pages/Login.jsx
+++ myApp/src/pages/Login.jsx
@@ -1,7 +1,57 @@
+import { useState } from "react"
+
 function Login() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleLogin(e) {
+    e.preventDefault()
+
+    console.log(email)
+    console.log(password)
+  }
+
   return (
-    <div className="text-4xl text-center mt-20">
-      Login Page
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+
+      <div className="bg-white p-10 rounded-2xl shadow-lg w-400px">
+
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Login
+        </h1>
+
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col gap-5"
+        >
+
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="border p-3 rounded-lg outline-none"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="border p-3 rounded-lg outline-none"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button
+            className="bg-blue-900 text-white py-3 rounded-lg hover:bg-blue-700 transition"
+          >
+            Login
+          </button>
+
+        </form>
+
+      </div>
+
     </div>
   )
 }
--- myApp/src/pages/Register.jsx
+++ myApp/src/pages/Register.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react"
+
 function Register() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleRegister(e) {
+    e.preventDefault()
+
+    console.log(name)
+    console.log(email)
+    console.log(password)
+  }
+
   return (
-    <div className="text-4xl text-center mt-20">
-      Register Page
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+
+      <div className="bg-white p-10 rounded-2xl shadow-lg w-400px">
+
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Register
+        </h1>
+
+        <form
+          onSubmit={handleRegister}
+          className="flex flex-col gap-5"
+        >
+
+          <input
+            type="text"
+            placeholder="Enter Name"
+            className="border p-3 rounded-lg outline-none"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="border p-3 rounded-lg outline-none"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="border p-3 rounded-lg outline-none"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button
+            className="bg-green-700 text-white py-3 rounded-lg hover:bg-green-600 transition"
+          >
+            Register
+          </button>
+
+        </form>
+
+      </div>
+
     </div>
   )
 }
